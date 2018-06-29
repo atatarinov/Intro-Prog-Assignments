bcb4daa0dbda42e98f31cd28d9a7bf75f8d10817
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-06-29 10:48:07 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        ExerciseClass tasks = new ExerciseClass();
+
+        tasks.printAsterisk();
+    }
+}
---
+++ src/ExerciseClass.java
@@ -0,0 +1,9 @@
+public class ExerciseClass {
+
+    public void printAsterisk() {
+        System.out.println("*");
+    }
+
+    public void printHorizontalLine(int n) {
+    }
+}
